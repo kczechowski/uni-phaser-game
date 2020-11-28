3951e9e8b9e4078e645f5031e854c86b9d6cdb23
KislyKamil <kislykamil@gmail.com>
2020-11-28 19:08:35 +0100

    added menu listner

--- src/game.js
+++ src/game.js
@@ -5,7 +5,7 @@ import IsoPlugin from 'phaser3-plugin-isometric';
 import { Column, TextSprite, TextButton } from 'phaser-ui-tools';
 
 import { GameManager } from "./GameManager";
-import { RoadMapObject } from "./map";
+import { ElectricityMapObject, IndustrialBuildingMapObject, MarketBuildingMapObject, ResidentialBuildingMapObject, RoadMapObject, WaterMapObject } from "./map";
 
 
 class IsoInteractionExample extends Scene {
@@ -22,6 +22,7 @@ class IsoInteractionExample extends Scene {
         this._gameManager = new GameManager();
         console.log(this._gameManager);
     }
+
 
     preload() {
         var assetRoot = './../dist/assets/';
@@ -61,7 +62,6 @@ class IsoInteractionExample extends Scene {
     }
 
     createMap() {
-
         this.isoGroup.clear(true, true);
 
         const tileSize = 42;
@@ -118,28 +118,31 @@ class IsoInteractionExample extends Scene {
         var textStyle = { 'fill': '#FFF', 'font': '16px Courier New' };
         this.header = new TextSprite(this, 0, 0, 'header').setText('SimCityPAW', textStyle).setOrigin(0.0, 0.0);
 
-        var road = new TextButton(this, 0, 0, 'button', buttonHandler, this, 1, 0, 2, 1)
+        var road = new TextButton(this, 0, 0, 'button', () => { selectBuildingHandler('Road', this.header, this._gameManager); }, this, 1, 0, 2, 1)
             .setText('Road', textStyle)
             .eventTextYAdjustment(3);
-        var buliding1 = new TextButton(this, 0, 0, 'button', null, this, 1, 0, 2, 1)
-            .setText('bd', textStyle)
+        var buliding1 = new TextButton(this, 0, 0, 'button', () => { selectBuildingHandler('Residential Zone', this.header, this._gameManager); }, this, 1, 0, 2, 1)
+            .setText('Residential Zone', textStyle)
             .eventTextYAdjustment(3);
-        var building2 = new TextButton(this, 0, 0, 'button', null, this, 1, 0, 2, 1)
-            .setText('bd1', textStyle)
+        var building2 = new TextButton(this, 0, 0, 'button', () => { selectBuildingHandler('Market Zone', this.header, this._gameManager); }, this, 1, 0, 2, 1)
+            .setText('Market Zone', textStyle)
             .eventTextYAdjustment(3);
-        var building3 = new TextButton(this, 0, 0, 'button', null, this, 1, 0, 2, 1)
-            .setText('bd2', textStyle)
+        var building3 = new TextButton(this, 0, 0, 'button', () => { selectBuildingHandler('Industrial Zone', this.header, this._gameManager); }, this, 1, 0, 2, 1)
+            .setText('Industrial Zone', textStyle)
             .eventTextYAdjustment(3);
-        var building4 = new TextButton(this, 0, 0, 'button', null, this, 1, 0, 2, 1)
-            .setText('bd3', textStyle)
+        var building4 = new TextButton(this, 0, 0, 'button', () => { selectBuildingHandler('Water', this.header, this._gameManager); }, this, 1, 0, 2, 1)
+            .setText('Water', textStyle)
             .eventTextYAdjustment(3);
-
+        var building5 = new TextButton(this, 0, 0, 'button', () => { selectBuildingHandler('Electricity', this.header, this._gameManager); }, this, 1, 0, 2, 1)
+            .setText('Electricity', textStyle)
+            .eventTextYAdjustment(3);
         var column = new Column(this, 190, 100);
         column.addNode(road, 0, 5);
         column.addNode(buliding1, 0, 5);
         column.addNode(building2, 0, 5);
         column.addNode(building3, 0, 5);
         column.addNode(building4, 0, 5);
+        column.addNode(building5, 0, 5);
     }
 
 }
@@ -162,8 +165,34 @@ function resize(gameSize) {
     this.cameras.resize(width, height);
 }
 
-function buttonHandler() {
-    this.header.setText('Clicked').setOrigin(0.0, 0.0);
-}
+const selectBuildingHandler = (type, header, _gameManager) => {
+    header.setText('Choosed ' + type).setOrigin(0.0, 0.0);
+
+    switch (type) {
+        case 'Road':
+            _gameManager.setCurrentBlock(new RoadMapObject());
+            break;
+
+        case 'Residential Zone':
+            _gameManager.setCurrentBlock(new ResidentialBuildingMapObject());
+            break;
+
+        case 'Market Zone':
+            _gameManager.setCurrentBlock(new MarketBuildingMapObject());
+            break;
+
+        case 'Industrial Zone':
+            _gameManager.setCurrentBlock(new IndustrialBuildingMapObject());
+            break;
+
+        case 'Water':
+            _gameManager.setCurrentBlock(new WaterMapObject());
+            break;
+
+        case 'Electricity':
+            _gameManager.setCurrentBlock(new ElectricityMapObject());
+            break;
+    }
+};
 
 const game = new Game(config);
